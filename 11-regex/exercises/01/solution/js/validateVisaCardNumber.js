907dc8e6b6a730fde6449a7582ad75f2f0dcf4b0
zsuzsasovari <105643090+zsuzsasovari@users.noreply.github.com>
2022-10-22 12:26:17 +0200

    kész

--- 11-regex/exercises/01/solution/js/validateVisaCardNumber.js
+++ 11-regex/exercises/01/solution/js/validateVisaCardNumber.js
@@ -1,1 +1,13 @@
+// 1. Írj egy függvényt `validateVisaCardNumber` néven, ami paraméterként egy visa kártyaszámot vár stringként, szóközök nélkül!   
+// A függvény egy `Boolean` értékben visszaadja, hogy a paraméterként megadott kártyaszám megfelelő formátumú e, vagy sem.
+
+const validateVisaCardNumber = (cardnumber) => {
+    const visaRegExp = /^4[0-9]{12}(?:[0-9]{3})?$/;
+    if (visaRegExp.test(cardnumber)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 export default validateVisaCardNumber;
--- 11-regex/exercises/03/solution/js/validateMACAddress.js
+++ 11-regex/exercises/03/solution/js/validateMACAddress.js
@@ -1,1 +1,12 @@
+// 3. Írj  egy függvényt `validateMACAddress`, ami paraméterként egy MAC címet vár stringként!   
+// A függvény egy `bollean` értékben visszaadja, hogy a paraméterként megadott MAC cím megfelelő formátumú e, vagy sem.
+const validateMACAddress = (address) => {
+    const mac = /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/;
+    if (mac.test(address)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 export default validateMACAddress;
